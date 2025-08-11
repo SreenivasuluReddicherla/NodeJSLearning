25eda7e34b8cd6bbb1ce3ad1c490733426f9647f
SreenivasuluReddicherla <sreenivasreddicherla@gmail.com>
2025-08-11 23:08:23 +0530

    first end point

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Welcome to Node JS Learning ')
+//import the express module
+const express = require('express');
+// Define the port number the server will listen on
+const PORT = 3000;
+// create an instance for  express application
+// because it give us the starting  point
+const app = express();
+app.get("/hello",(req, res)=>{
+    res.send("hello world");
+});
+// start the server and listen on the specified port
+app.listen(PORT,"0.0.0.0", function(){
+    //LOG THE NUMBER
+    console.log(`server is running on ${PORT}`);
+})
