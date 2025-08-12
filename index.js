8767bbc20054439ac154dbd711993645b6649e76
SreenivasuluReddicherla <sreenivasreddicherla@gmail.com>
2025-08-12 21:38:24 +0530

    Seperate routes

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 //import the express module
 const express = require('express');
+
+const helloRoute = require('./routes/hello')
 // Define the port number the server will listen on
 const PORT = 3000;
 // create an instance for  express application
 // because it give us the starting  point
 const app = express();
-app.get("/hello",(req, res)=>{
-    res.send("hello world");
-});
+
+// app.get("/hello",(req, res)=>{
+//     res.send("hello world");
+// });
+//middleware to register routes or to mount routes
+app.use('/',helloRoute);
 // start the server and listen on the specified port
 app.listen(PORT,"0.0.0.0", function(){
     //LOG THE NUMBER
     console.log(`server is running on ${PORT}`);
-})
+});
