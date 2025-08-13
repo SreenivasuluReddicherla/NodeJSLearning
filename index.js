40f83d7934f174181ecd86d3df391403e4342a93
SreenivasuluReddicherla <sreenivasreddicherla@gmail.com>
2025-08-13 23:10:57 +0530

    Mongodb connection

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 //import the express module
 const express = require('express');
+const helloRoute = require('./routes/hello');
+const mongoose  = require('mongoose');
 
-const helloRoute = require('./routes/hello')
 // Define the port number the server will listen on
 const PORT = 3000;
 // create an instance for  express application
 // because it give us the starting  point
 const app = express();
-
+// mangodb string
+const DB = "mongodb+srv://sreenivasreddicherla:Sreenivas@cluster0.3bpnjsc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 // app.get("/hello",(req, res)=>{
 //     res.send("hello world");
 // });
 //middleware to register routes or to mount routes
 app.use('/',helloRoute);
+mongoose.connect(DB).then(()=>{
+    console.log('mongodb connected');
+});
 // start the server and listen on the specified port
 app.listen(PORT,"0.0.0.0", function(){
     //LOG THE NUMBER
